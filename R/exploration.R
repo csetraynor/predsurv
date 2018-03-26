8abb907bf12e858c8cc6197ec0cc6387a7ff4e93
csetr <carlos.serra91@gmail.com>
2018-03-26 21:30:47 +0100

    sample pro

--- R/exploration.R
+++ R/exploration.R
@@ -11,12 +11,14 @@
 plot_tte_dist <- function(d, time = os_months, status = os_status, alpha = 0.5){
   time <- dplyr::enquo(time)
   status <- dplyr::enquo(status)
-
+  theme_set(theme_bw())
   p <- d %>%
     ggplot(aes_string(x = rlang::quo_text(time),
-               group = rlang::quo_text(status),
-               colour = rlang::quo_text(status),
-               fill = rlang::quo_text(status) )) +
-    geom_density(alpha = alpha)
-  p
+                      group = rlang::quo_text(status),
+                      colour = rlang::quo_text(status),
+                      fill =  rlang::quo_text(status))) +
+    geom_density( alpha = alpha)+
+    scale_color_discrete(name = "Overall Survival", label=c("Deceased", "Living")) +
+    scale_fill_discrete(name = "Overall Survival", label=c("Deceased", "Living"))
+  p + labs(x = "Time (months)", title= "Time to event density distribution")
 }
