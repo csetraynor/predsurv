fd2b640755d46f0e3f79b5dd15458706c999f650
csetraynor <c.traynor@warwick.ac.uk>
2018-04-28 18:48:19 +0100

    mymodels

--- R/pred_surv.R
+++ R/pred_surv.R
@@ -20,54 +20,62 @@
 #' @import doMC
 
 fun_train <- function(train, time = os_months, status = os_deceased, fit, penalty = "BIC" , iterative = FALSE){
+
+  ###### abstract dependent vars
   time <- dplyr::enquo(time)
   status <- dplyr::enquo(status)
 
+  ##### create training dataset
   train <- as.data.frame(train)
   if('subject' %in% colnames(train)){
     train <- train %>% dplyr::select(-subject)
   }
   # take only covariates for which beta is not zero
   trainX <- train %>% dplyr::select(-!!time, -!!status)
-
   traincoxphdata <- cbind(train %>% dplyr::select(!!time, !!status), trainX)
 
-  #create formula
+  ##### create formula
    form <- as.formula(paste("Surv(time, status)", paste("~", paste(names(trainX), collapse = " + "), sep = " " )))
 
-   #create fold id for CV
+   ###### create fold id for CV in glmnet
    set.seed(9)
    foldid <-  caret::createFolds(train %>% select(!!status) %>% unlist,
                                  k = 10, list = FALSE)
-   #Fit models
+   ###### Fit models
+   #### Univariate
   if(fit == "Univariate" | iterative){
+    #create formula for apply loop
     reg <- function(indep_var,dep_var,data_source) {
       formula <- as.formula(paste(dep_var," ~ ", indep_var))
       res     <- survival::coxph(formula, data = data_source)
       summary(res)
     }
 
+    #### fit univariate cox models with each covariate
     mod1 <- lapply(colnames(trainX), FUN = reg, dep_var = "Surv(time, status)", data_source = traincoxphdata %>% dplyr::mutate(
       time   = !!time,
       status = !!status) )
-
     if(iterative){
+      #soft correction
       mod2 <- lapply(mod1, function(p) p[p$logtest["pvalue"] < 0.001]$coefficients)
     }else{
       #Bonferroni correction
       mod2 <- lapply(mod1, function(p) p[p$logtest["pvalue"] < 0.0005]$coefficients)
     }
+    ### drop covariates that are not "statistically significant"
     mod2[sapply(mod2, is.null)] <- NULL
     features <- sapply(mod2, function(p) rownames(p))
     coefficients <- sapply(mod2, function(p) p[,1])
     mod <- data.frame(coef = coefficients)
     rownames(mod) <- features
     # print(mod)
+
     if(iterative & length(mod) > 1){
+      # change to elastic net
       fit <-  "Elastic net"
     }
   }
-
+   #Stepwise fit, currently not recommended computationally too expensive
   if(fit == "Stepwise"){
     # create coxph object
     fit1 <- suppressWarnings(survival::coxph(Surv(time, status)~1,
@@ -91,56 +99,69 @@ fun_train <- function(train, time = os_months, status = os_deceased, fit, penalt
             k = k,
             steps =  nrow(train)-1 ))
   }
+   #Lasso
   if(fit == "Lasso" | fit == "Adaptive Lasso"){
+    #prepare data for glmlasso
     x <- as.matrix(trainX)
     y <- as.matrix(train %>%
                      dplyr::select(time = !!time, status = !!status), ncol = 2)
-    #register do Parallel
+    #register for parallelisation cv
     doMC::registerDoMC(cores=parallel::detectCores() - 1)
+    #fit glmlasso
     mod <-  glmnet::cv.glmnet(x, y, family = "cox", grouped = TRUE, lambda.min.ratio = 0.001, foldid = foldid, parallel = TRUE)
 
-    # find lambda for which dev.ratio is max
+    # find optimised lambda
     optimal.coef <- as.matrix(coef(mod, s = "lambda.min"))
     optimal.coef <- as.data.frame(optimal.coef)
     colnames(optimal.coef) <- "mod"
     optimal.coef <- tibble::rownames_to_column(optimal.coef, var = "gene")
-
     optimal.coef <-  optimal.coef[optimal.coef$mod != 0,]
     mod <- data.frame(coef = optimal.coef$mod)
     rownames(mod) <- optimal.coef$gene
   }
 
   if(fit == "Ridge regression"){
+    #prepare for ridge regression
      x <- as.matrix(trainX)
      y <- as.matrix(train %>%
                       dplyr::select(time = !!time, status = !!status), ncol = 2)
      #register do Parallel
      doMC::registerDoMC(cores=parallel::detectCores() - 1)
      mod <-  glmnet::cv.glmnet(x, y, family = "cox", alpha = 0 ,grouped = TRUE, lambda.min.ratio = 0.001, foldid = foldid, parallel = TRUE) #alpha 0 ridge penalty
-     # find lambda for which dev.ratio is max
-     mod <-
-     optimal.coef <- as.matrix(coef(mod, s = "lambda.min") )
-     mod <-  optimal.coef[optimal.coef != 0,]
-     mod <- as.data.frame(mod)
+     # find optimised lambda
+     optimal.coef <- as.matrix(coef(mod, s = "lambda.min"))
+     optimal.coef <- as.data.frame(optimal.coef)
+     colnames(optimal.coef) <- "mod"
+     optimal.coef <- tibble::rownames_to_column(optimal.coef, var = "gene")
+
+     optimal.coef <-  optimal.coef[optimal.coef$mod != 0,]
+     mod <- data.frame(coef = optimal.coef$mod)
+     rownames(mod) <- optimal.coef$gene
    }
    if(fit == "Elastic net"){
+     #create a sequence of alpha
      alphaList <-  (1:19) * 0.05
      if(iterative){
        # print(mod)
+       # obtain the subset of covariates from univariate
        x <- as.matrix(trainX %>% select(rownames(mod)))
      }else{
+       #prepare for glmnet
        x <- as.matrix(trainX)
      }
     y <- as.matrix(train %>%
                         dplyr::select(time = !!time, status = !!status), ncol = 2)
-    #register do Parallel
+    #register for parallelisation
     doMC::registerDoMC(cores=parallel::detectCores()-1)
+    #do crossvalidation to find optimal alpha and lambda
        elasticnet <-  lapply(alphaList, function(a){
          glmnet::cv.glmnet(x, y, family = "cox", grouped = TRUE , alpha = a, lambda.min.ratio = 0.001, foldid = foldid, parallel = TRUE)});
+       #extract optimal lambda
        cvm <- sapply(seq_along(alphaList), function(i) min(elasticnet[[i]]$cvm ) );
-
+       doParallel::stopImplicitCluster()
        a <- alphaList[match(min(cvm), cvm)];
        mod <- elasticnet[[match(min(cvm), cvm)]]
+
        # find lambda for which dev.ratio is max
        optimal.coef <- as.matrix(coef(mod, s = "lambda.min"))
        optimal.coef <- as.data.frame(optimal.coef)
@@ -167,12 +188,13 @@ fun_train <- function(train, time = os_months, status = os_deceased, fit, penalt
                     dplyr::mutate(
                       time   = !!time,
                       status = !!status) )
-    forest_control <- party::cforest_unbiased(n = 500)
+    forest_control <- party::cforest_unbiased()
 
     mod <- pec::pecCforest(form,
                            data = train_tree,
                           controls = forest_control)
   }
+   #currently not recommended either PCR or PLS because predictions are "tricky"
   if(fit == "PLS"){
     X_train <- apply((as.matrix(trainX)), FUN="as.numeric",MARGIN = 2)
     Y_train <- train %>% dplyr::select(time = !!time) %>% unlist %>% as.numeric
@@ -247,11 +269,13 @@ fun_test <- function(obj, train_data = train, data = lungdata, subject = subject
         names(selectedBeta) <- rownames(selectedBeta)
       }
     }
+
+  if(fit != "Random forest"){
     ##### No variables selected complete "shrinkage" tipycal in Lasso
-    if(length(selectedBeta) == 0 & fit != "Random forest"){
+    if(length(selectedBeta) == 0 ){
       mod <-  survival::coxph(Surv(time, status)~1, iter = 0,
                               data = train_data %>%
-                              dplyr::mutate(time = !!time, status = !!status))
+                                dplyr::mutate(time = !!time, status = !!status))
       # Create Test vars
       testX <- test_data %>% dplyr::select(-!!time, -!!status)
       ndata <- test_data %>% dplyr::mutate(time = !!time, status =  !!status)
@@ -269,7 +293,7 @@ fun_test <- function(obj, train_data = train, data = lungdata, subject = subject
       # }
     }
     #Start normal prediction
-    if(length(selectedBeta) > 0 & fit != "Random forest"){
+    if(length(selectedBeta) > 0 ){
       # Create train X take only covariates for which beta is not zero
       trainX <- train_data %>% dplyr::select(-!!time, -!!status)
       selectedTrainX   <- as.data.frame(trainX[,colnames(trainX) %in% selectedBeta])
@@ -301,105 +325,113 @@ fun_test <- function(obj, train_data = train, data = lungdata, subject = subject
       colnames(selectedTestX) <- selectedBeta
       ndata <- cbind(test_data %>% dplyr::select(!!time, !!status) , selectedTestX)
     }
-      #Create grid of equidistant time points for testing
-      timepoints <-  seq(0, max(test_data %>%
-                                    dplyr::filter(!!status == event_type) %>%
-                                    dplyr::select(!!time) %>% unlist
-      ) , length.out = 100L)
-      ####Prediction Error
-      if(pred == "Brier" | all){
-        if(length(selectedBeta) == 0){
-          probs <- matrix(runif(nrow(test_data)*100), ncol = 100)
-          brier <- pec::pec(probs, Surv(time, status) ~ 1,
-                            data = test_data %>%
+    #Create grid of equidistant time points for testing
+    timepoints <-  seq(0, max(test_data %>%
+                                dplyr::filter(!!status == event_type) %>%
+                                dplyr::select(!!time) %>% unlist
+    ) , length.out = 100L)
+    ####Prediction Error
+    if(pred == "Brier" | all){
+      if(length(selectedBeta) == 0){
+        probs <- matrix(runif(nrow(test_data)*100), ncol = 100)
+        brier <- pec::pec(probs, Surv(time, status) ~ 1,
+                          data = test_data %>%
                             dplyr::mutate( time   = !!time, status = !!status),
-                            maxtime = max(timepoints),
-                            exact = F,
-                            exactness = 99L)
-          out <- brier
-          if(integrated){
-            out <- pec::crps(brier, models = "Reference")[1]
-          }
-        }else{
-          #Calculate probs
-          probs <- pec::predictSurvProb(mod,
-                                        newdata = ndata, times = timepoints)
-          #Calculate brier score
-          brier <- pec::pec(probs, Surv(time, status) ~ 1,
-                            data = ndata %>%
+                          maxtime = max(timepoints),
+                          exact = F,
+                          exactness = 99L)
+        out <- brier
+        if(integrated){
+          out <- pec::crps(brier, models = "Reference")[1]
+        }
+      }else{
+        #Calculate probs
+        probs <- pec::predictSurvProb(mod,
+                                      newdata = ndata, times = timepoints)
+        #Calculate brier score
+        brier <- pec::pec(probs, Surv(time, status) ~ 1,
+                          data = ndata %>%
                             dplyr::mutate( time = !!time, status = !!status),
-                            maxtime = max(timepoints),
-                            exact = F,
-                            exactness = 99L)
-          out <- brier
-          if(integrated){
-            out <- predsurv::fun_ibrier_score(out)
-          }
-        }
-      }
-      if(pred == "ROC" | all ){
-        if(length(selectedBeta) == 0){
-          probs <- rep(0, nrow(testX))
-          names(probs) <- rownames(testX)
-        }else{
-          # if(fit == "Lasso" |  fit == "Elastic net" | fit == "Ridge regression"){
-          #   probs <- as.matrix(testX, ncol = length(selectedBeta)) %*% obj
-          # }else{
-            probs <- predict(mod, newdata = testX, type = "lp")
-          # }
-        }
-        roc <- tdROC::tdROC(X = probs,
-                            Y = test_data %>% dplyr::select(time = !!time)%>% unlist,
-                            delta = test_data %>% dplyr::select(status = !!status)%>% unlist,
-                            tau = quantile(test_data %>% dplyr::select(time = !!time)%>% unlist, .73), nboot = 0, alpha = 0.05, n.grid = 1000,  type = "uniform"
-        )
-        out <- roc
+                          maxtime = max(timepoints),
+                          exact = F,
+                          exactness = 99L)
+        out <- brier
         if(integrated){
-          out <- out$AUC[1] %>% unlist
+          out <- predsurv::fun_ibrier_score(out)
         }
       }
-      if(pred  == "Deviance" | all  ){
-        logl <- -2*(mod$loglik)
-        out <- logl
-        if(integrated){
-          out <- out[2]
-        }
-      }
-      if(pred == "c_index" | all ){
-        if(length(selectedBeta) == 0){
-          out <- 0.5
+    }
+    if(pred == "ROC" | all ){
+      if(length(selectedBeta) == 0){
+        probs <- rep(0, nrow(testX))
+        names(probs) <- rownames(testX)
+      }else{
+        if(fit == "Ridge regression"){
+          probs <- as.matrix(testX, ncol = length(selectedBeta)) %*% as.vector(unlist(obj))
         }else{
-          ###Create your survival estimates
-          ci <-  pec::cindex(mod, formula = Surv(time, status) ~ 1,
-                             data = ndata %>% dplyr::mutate(
-                               time   = !!time, status = !!status))
-          out <- ci
-          return(out)
-          if(integrated){
-            out <-  out$AppCindex$coxph
-          }
+          probs <- predict(mod, newdata = testX, type = "lp")
         }
       }
+      roc <- tdROC::tdROC(X = probs,
+                          Y = test_data %>% dplyr::select(time = !!time)%>% unlist,
+                          delta = test_data %>% dplyr::select(status = !!status)%>% unlist,
+                          tau = quantile(test_data %>% dplyr::select(time = !!time)%>% unlist, .73), nboot = 0, alpha = 0.05, n.grid = 1000,  type = "uniform"
+      )
+      out <- roc
+      if(integrated){
+        out <- out$AUC[1] %>% unlist
+      }
+    }
+    if(pred  == "Deviance" | all  ){
+      logl <- -2*(mod$loglik)
+      out <- logl
+      if(integrated){
+        out <- out[2]
+      }
+    }
+    if(pred == "c_index" | all ){
+      if(length(selectedBeta) == 0){
+        out <- 0.5
+      }else{
+        ###Create your survival estimates
+        ci <-  pec::cindex(mod, formula = Surv(time, status) ~ 1,
+                           data = ndata %>% dplyr::mutate(
+                             time   = !!time, status = !!status))
+        out <- ci
+
+        if(integrated){
+          out <-  out$AppCindex$coxph
+        }
+      }
+    }
+  }else{
     if(fit == "Random forest"){
       # Create Test vars
       testX <- test_data %>% dplyr::select(-!!time, -!!status)
       ndata <- cbind(test_data %>% dplyr::select(!!time, !!status), testX)
       #Create grid of equidistant time points for testing
       timepoints <-  seq(0,
-              max(train_data %>%
-                dplyr::filter(!!status == event_type) %>%
-                dplyr::select(!!time) %>% unlist), length.out = 100L)
+                         max(test_data %>%
+                               dplyr::filter(!!status == event_type) %>%
+                               dplyr::select(!!time) %>% unlist), length.out = 100L)
       #Calculate probs
       mod <- obj;
       probs <- pec::predictSurvProb(mod,
                                     newdata = ndata, times = timepoints)
       #Calculate brier score
       out <- pec::pec(probs, Surv(time, status) ~ 1,
-                        data = ndata %>% dplyr::mutate( time   = !!time, status = !!status),
-                        maxtime = max(timepoints),
-                        exact = F,
-                        exactness = 99L)
+                      data = ndata %>% dplyr::mutate( time   = !!time, status = !!status),
+                      maxtime = max(timepoints),
+                      exact = F,
+                      exactness = 99L)
+      if(integrated){
+        out <- predsurv::fun_ibrier_score(out)
+      }
+    }else{
+      print("Not accepted fit")
     }
+  }
+
   if(all & fit != "Random forest"){
     attr(out, 'brier_pred') <- brier
     attr(out, 'ci_pred') <- ci
@@ -448,7 +480,7 @@ fun_ibrier_score <- function(brier){
 #' @import prodlim
 fun_score <- function(train, fit, data, prediction){
 
-  obj <- predsurv::fun_train( fit = "Univariate", train = train)
+  obj <- predsurv::fun_train( fit = fit, train = train)
   out <- predsurv::fun_test(obj = obj, train_data = train, data = lungdata, pred = prediction)
 
   return(out)
--- vignettes/cross_validation.R
+++ vignettes/cross_validation.R
@@ -1,4 +1,5 @@
 ##Cross validation
+memory.limit(5e10)
 data("lungdata")
 lungdata <- tibble::rownames_to_column(lungdata, var = "subject")
 
@@ -133,25 +134,57 @@ mc_samp$brier_iter_enet <- NULL
 
 
 ##Random forest
-mc_samp$mod_bst <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Random forest")
 
-mc_samp$brier_bst <- purrr::pmap_dbl(list(mc_samp$splits, mc_samp$mod_bst),
-                                      function(splits, mod){predsurv::fun_test(
-                                        obj = mod,
-                                        train_data = splits,
-                                        data = lungdata,
-                                        pred = "Brier"
-                                      )
-                                      })
-saveRDS(mc_samp$mod_bst, "mod_bst.RDS")
-mc_samp$mod_bst <- NULL
+mc_samp$brier_bst <- purrr::map(mc_samp$splits, predsurv::fun_score, fit = "Random forest",
+                              data = lungdata, prediction = "Brier")
+
+
 saveRDS(mc_samp$brier_bst, "brier_bst.RDS")
 mc_samp$brier_bst <- NULL
 
 
 
-mc_samp$brier_uni <- purrr::map_dbl(mc_samp$splits, predsurv::fun_score, fit = "Univariate", data = lungdata, prediction = "Brier")
-mc_samp$brier_lasso <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Lasso")
-mc_samp$brier_lasso <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Elastic net")
-mc_samp$brier_iter <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Univariate", iterative = TRUE)
-mc_samp$brier_bst  <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Random forest")
+# mc_samp$brier_uni <- purrr::map_dbl(mc_samp$splits, predsurv::fun_score, fit = "Univariate", data = lungdata, prediction = "Brier")
+# mc_samp$brier_lasso <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Lasso")
+# mc_samp$brier_lasso <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Elastic net")
+# mc_samp$brier_iter <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Univariate", iterative = TRUE)
+# mc_samp$brier_bst  <- purrr::map(mc_samp$splits, predsurv::fun_train, fit = "Random forest")
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+mc_samp$uni <- readRDS("brier_uni.RDS")
+mc_samp$lasso <- readRDS("brier_lasso.RDS")
+mc_samp$enet <- readRDS("brier_enet.RDS")
+mc_samp$iter_enet <- readRDS("brier_iter_enet.RDS")
+mc_samp$bst <- readRDS("brier_bst.RDS")
+
+
+mc_samp$uni <- readRDS("roc_uni.RDS")
+mc_samp$lasso <- readRDS("roc_lasso.RDS")
+mc_samp$enet <- readRDS("roc_enet.RDS")
+mc_samp$iter_enet <- readRDS("roc_iter_enet.RDS")
+mc_samp$ridge <- readRDS("roc_ridge.RDS")
+
+mc_samp %>%
+  select(-splits) %>%
+  gather() %>%
+  ggplot(aes(x = statistic, col = model)) +
+  geom_line(stat = "density") +
+  theme_bw() +
+  theme(legend.position = "top")
+
+library(tidyposterior)
+mc_samp <- perf_mod(mc_samp, seed = 6507, iter = 5000)
+
+ggplot(tidy(mc_samp)) +
+  theme_bw()
+
+
+comparisons <- contrast_models(
+  mc_samp,
+  list_1 = rep("full", 3),
+  list_2 = c("ph.ecog", "age", "sex"),
+  seed = 4654
+)
